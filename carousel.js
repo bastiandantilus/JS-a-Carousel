0b307ea5fcb7e78dc275c26fdf560db34d650332
Scott Robert Lawrence <theelven@gmail.com>
2014-03-31 14:34:19 -0700

    Base flexy css for thunbnail shininess and xhr for image loading

--- carousel.js
+++ carousel.js
@@ -1,3 +1,37 @@
 /**
  * @author Scott
  */
+(function(){
+	
+	function get_image_data(thendo) {
+		function readystatechange() {
+			if (this.readyState == 4 && this.status == 200){
+				console.log(this);
+				thendo(this.responseText);
+			}			
+			
+		};
+		
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = readystatechange;
+		
+		//@TODO To extend this functionality, create the json server side
+		xhr.open("get", "images.json", true);
+		xhr.send();
+		
+	};
+	
+	function display_gallery(data) {
+		var div, img, gallery = document.getElementById("gallery");
+		var images = JSON.parse(data)['data'];
+		for(var i = 0, len = images.length; i < len ; i++) {
+			div = document.createElement("div");
+			div.className = "thumb";
+			div.style.backgroundImage = "url('images/"+images[i]+"')";
+			gallery.appendChild(div);
+		}
+	};
+	
+	get_image_data(display_gallery);
+	
+})();
